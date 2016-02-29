390c3bea1d8161b9a873d22b915c1ce5057d8529
AVChkadua <anton60.98@gmail.com>
2016-02-29 19:51:01 +0300

    Задача со стеком

--- stack.rb
+++ stack.rb
@@ -1,24 +1,42 @@
-def try(sequence)
+def try(amount, sequence)
+  seq = sequence.split(" ")
+  0.upto(amount.to_i - 1) do |i|
+    seq[i] = seq[i].to_i
+  end
   stack = Array.new
   i = 0
-  while i < sequence.length - 1
-    if sequence[i] > sequence[i + 1]
-      stack.push(sequence.delete_at(i))
-      if stack.length > 1 and stack[-1] > stack[-2] then return 1 end
-    elsif sequence[i] > stack[-1]
-      stack.pop
+  j = 1
+  while j < amount.to_i + 1
+    if seq[i] != j
+      if stack[-1] != nil and stack[-1] == j
+        stack.pop
+        j += 1
+      elsif stack[-1] != nil and seq[i] != nil and stack[-1] < seq[i]
+        return 1
+      else
+        if seq[i] != nil
+          stack.push(seq.delete_at(i))
+        end
+      end
     else
       i += 1
+      j += 1
     end
   end
   return 0
 end
 
 amount = gets
-sequence = gets
-seq = sequence.split(" ")
-if try(seq) == 0
-  puts "yes"
-else
-  puts "no"
+tasks = Array.new
+while !amount.eql?("0\n")
+  sequence = gets
+  tasks += [[amount, sequence]]
+  amount = gets
+end
+tasks.each do |elem|
+  if try(elem[0], elem[1]) == 0
+    puts "yes"
+  else
+    puts "no"
+  end
 end
